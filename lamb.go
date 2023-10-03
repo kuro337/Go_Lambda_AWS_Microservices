867a8b4dd305c6ff3b38b445534dea8bc2e58c0d
kuro337 <65412787+kuro337@users.noreply.github.com>
2023-10-03 02:17:40 -0400

    Update lamb.go

--- lamb.go
+++ lamb.go
@@ -16,7 +16,7 @@ func getLabels() (*rekognition.DetectLabelsOutput, error) {
 	input := &rekognition.DetectLabelsInput{
 Image: &rekognition.Image{
 	S3Object: &rekognition.S3Object{
-			Bucket: aws.String("testbucketchinmay"),
+			Bucket: aws.String("testbucket"),
 			Name:   aws.String("happy-dog-wears-flowers.jpg"),
 	},
 },
